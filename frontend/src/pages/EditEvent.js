30832e4752b04d625a2cba837568dfbb2026b58e
Sagar Mittal <mittalsagar006@gmail.com>
2023-01-31 15:51:00 +0530

    useRouteLoader() hook & accessing data from other routes

--- frontend/src/pages/EditEvent.js
+++ frontend/src/pages/EditEvent.js
@@ -1,7 +1,14 @@
+import { useRouteLoaderData } from 'react-router-dom';
+
+import EventForm from '../components/EventForm';
+
 const EditEventPage = () => {
+  const data = useRouteLoaderData('event-detail');
+  const event = data.event;
+
   return (
     <>
-      <h1>Edit Event Page</h1>
+      <EventForm event={event} />
     </>
   );
 };
